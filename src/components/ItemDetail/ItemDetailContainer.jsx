c2f0acf196a4a7338fdb4ad5c0abc075c9e906dd
Guillermo Zumaran <gzumaranbazan@gmail.com>
2022-09-18 14:24:40 -0500

    implemented loader

--- src/components/ItemDetail/ItemDetailContainer.jsx
+++ src/components/ItemDetail/ItemDetailContainer.jsx
@@ -4,6 +4,7 @@ import { useEffect } from "react"
 import ItemDetail from "./ItemDetail"
 import { useParams } from 'react-router-dom'
 import { itemsMock } from "../../mocks"
+import Loader from '../Common/Loader'
 import './styles/itemDetailContainer.css'
 const ItemDetailContainer = () => {
   const { id } = useParams()
@@ -15,21 +16,25 @@ const ItemDetailContainer = () => {
     } else {
       setTimeout(() => { resolve('') }, 1000)
     }
-
   })
-
   const [item, setItem] = useState({})
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
+    setLoading(true)
     getItem.then((res) => {
       setItem(res)
+      setLoading(false)
     })
   }, [])
   return (
-    <div className={'item-detail-container'}>
-      <ItemDetail
-        item={item}
-      />
-    </div>
+    (loading) ?
+      <div style={{ position: 'absolute', width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <Loader />
+      </div>
+      :
+      <div className={'item-detail-container'}>
+        <ItemDetail item={item} />
+      </div>
   )
 }
 export default ItemDetailContainer
--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -2,32 +2,41 @@ import React, { useEffect, useState } from "react";
 import Item from "./Item";
 import { itemsMock } from "../../mocks"
 import './styles/itemList.css'
+import Loader from "../Common/Loader";
 const ItemList = () => {
   const itemPromise = new Promise((resolve, reject) => {
     setTimeout(() => { resolve(itemsMock) }, 1000)
   })
 
   const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
+    setLoading(true)
     itemPromise.then((res) => {
       setItems(res)
+      setLoading(false)
     })
   }, [])
 
   return (
-    <div className={'item-list-container'}>
-      {
-        items.map((item) => {
-          return (
-            <>
-              <Item key={item.id.toString()}
-                {...item} />
-            </>
-          );
-        })
-      }
-    </div>
+    (loading) ?
+      <div style={{ position: 'absolute', width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <Loader />
+      </div>
+      :
+      <div className={'item-list-container'}>
+        {
+          items.map((item) => {
+            return (
+              <>
+                <Item key={item.id.toString()}
+                  {...item} />
+              </>
+            );
+          })
+        }
+      </div>
   );
 }
 export default ItemList;
